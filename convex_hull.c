46417dfbda9c56bf1ad34be27b2cd92becce7858
InspBarn <gregoire.vekemans@student.uclouvain.be>
2020-11-17 14:04:54 +0100

    :bug: argsort function operational

--- convex_hull.c
+++ convex_hull.c
@@ -1,39 +1,41 @@
 #include "convex_hull.h"
 
-void jarvis_march(GLsizei N, GLfloat (*coord)[2], int* hull)
-{
-	GLfloat *x_pos = coord[0];
-	GLfloat *y_pos = coord[1];
-	// argsort(N, x_pos, hull);
-	for(GLint i=0; i<nPoints; i++) {
-		//printf("%.2f\t%d\n",coord[0][i],hull[i]);
-		printf("%.2f\t%.2f\n",coord[0][i],coord[1][i]);
-	}
-}
-
-static void argsort(GLsizei N, GLfloat* vector, int* sorted)
-{
-	GLfloat x;
-	for(int i=0; i<N; i++) {
-		x = vector[i];
-
-		int j = 0;
-		while(x>vector[sorted[j]]) {j++;}
-		
-		if(j==i) {sorted[i] = i;}
-		else {insert(i,j, sorted);}
-	}
-}
-
 static inline void insert(int N, int M, int* sorted)
 {
 	int prev, next;
-	prev = N;
-	for(int a=M; a<N-1; a++) {
+	next = N;
+	for(int a=M; a<N; a++) {
+		prev = next;
 		next = sorted[a];
 		sorted[a] = prev;
-		prev = next;
 	}
-	sorted[N] = prev;
+	sorted[N] = next;
+}
+
+static void argsort(int nPoints, GLfloat* vector, int* argsorted_list)
+{
+	argsorted_list[0] = 0;
+	int i,j; double x;
+	for(i=1; i<nPoints; i++) {
+		x = vector[i];
+		for (j=i; j>0; j--) {
+			if (x<vector[argsorted_list[j-1]]) {
+				argsorted_list[j] = argsorted_list[j-1];
+			} else {break;}
+		}
+		argsorted_list[j] = i;
+	}
+}
+
+void jarvis_march(GLsizei nPoints, GLfloat (*coord)[2], int* hull)
+{
+	GLfloat *x_pos = coord[0];
+	GLfloat *y_pos = coord[1];
+
+	int *argsorted_coord;
+	argsort(nPoints, x_pos, argsorted_coord);
+
+	int nHull = 0;
+	hull[nHull] = argsorted_coord[0];
 }
 
