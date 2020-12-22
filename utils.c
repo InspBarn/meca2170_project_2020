61d61e0a78d1f8bec4a223ee44dcf6bf242f8f03
InspBarn <gregoire.vekemans@student.uclouvain.be>
2020-12-22 10:07:16 +0100

    :truck: move some functions for clarity

---
+++ utils.c
@@ -0,0 +1,200 @@
+#include "utils.h"
+
+float distance(const float x1[2], const float x2[2]) {
+	return sqrt(pow(x1[0]-x2[0],2.) + pow(x1[1]-x2[1],2.));
+}
+
+void insert(int nPoints, int *vec, int arg, int idx)
+{
+	int prev, next;
+	prev = arg;
+	for(int i=idx; i<nPoints; i++) {
+		next = vec[i];
+		vec[i] = prev;
+		prev = next;
+	}
+	vec[nPoints] = next;
+}
+
+int find(int nPoints, float coord[][2], const float point[2])
+{
+	float eps = 5e-3, dst;
+	for(int i=0; i<nPoints; i++)
+		if (distance(coord[i], point) < eps)
+			return i;
+	return -1;
+}
+
+int argmin(int nPoints, float coord[][2], int axis)
+{
+	int argmin = 0;
+	float min = coord[0][axis];
+	for (int i=1; i<nPoints; i++) {
+		if (coord[i][axis]<min) {
+			argmin = i; min = coord[i][axis];
+		}
+	}
+	return argmin;
+}
+
+int argmax(int nPoints, float coord[][2], int axis)
+{
+	int argmax = 0;
+	float max = coord[0][axis];
+	for (int i=1; i<nPoints; i++) {
+		if (coord[i][axis]>max) {
+			argmax = i; max = coord[i][axis];
+		}
+	}
+	return argmax;
+}
+
+void argsort(int nPoints, float coord[][2], int axis, int* argsorted_list)
+{
+	struct sort array[nPoints];
+    for (int i = 0; i < nPoints; i++) {
+		array[i].value = coord[i][axis];
+		array[i].index = i;
+	}
+	// Sort objects array according to value maybe using 'qsort'
+	qsort(array, nPoints, sizeof(array[0]), cmp);
+	for (int i = 0; i < nPoints; i++)
+		argsorted_list[i] = array[i].index;
+	/*
+		argsorted_list[0] = 0;
+		int i,j; float x;
+		for(i=1; i<nPoints; i++) {
+			x = coord[i][axis];
+			for (j=i; j>0; j--) {
+				if (x<coord[argsorted_list[j-1]][axis]) {
+					argsorted_list[j] = argsorted_list[j-1];
+				} else {break;}
+			}
+			argsorted_list[j] = i;
+		}
+	*/
+}
+
+int argfind(int nPoints, int *vec, int idx)
+{
+	for(int i=0; i<nPoints; i++)
+		if(idx==vec[i])
+			return i;
+	return -1;
+}
+
+double direction(float x1[2], float x2[2], float x3[2])
+{
+	double area = (x1[0]*x2[1] - x2[0]*x1[1]) \
+				- (x1[0]*x3[1] - x3[0]*x1[1]) \
+				+ (x2[0]*x3[1] - x3[0]*x2[1]);
+	return area;
+}
+
+int cmp(const void *a, const void *b)
+{
+    struct sort *a1 = (struct sort *)a;
+    struct sort *a2 = (struct sort *)b;
+    if ((*a1).value > (*a2).value)
+        return -1;
+    else if ((*a1).value < (*a2).value)
+        return 1;
+    else
+        return 0;
+}
+
+
+/*
+-------------------------------------------------
+                   CONVEX HULL
+-------------------------------------------------
+*/
+
+void convex_hull_init(struct convex_hull_t *hull, int start, int stop, float coord[][2], int display)
+{
+	hull->display = display;
+	hull->Start = start;
+	hull->nPoints = stop-start;
+
+	hull->coord = (float(*)[2])malloc(sizeof(hull->coord[0])*hull->nPoints);
+	hull->hull_idxs = (int*)malloc(sizeof(int)*hull->nPoints);
+
+	for(int i=0; i<hull->nPoints; i++) {
+		hull->coord[i][0] = coord[start+i][0];
+		hull->coord[i][1] = coord[start+i][1];
+	}
+	hull->nHull = 1;
+	hull->hull_idxs[0] = 0;
+	// hull->nHull = hull_function(hull->nPoints, hull->coord, hull->hull_idxs);
+
+	if (display)
+		convex_hull_display_init(hull, -1);
+}
+
+void convex_hull_display_init(struct convex_hull_t *hull, int color)
+{
+	hull->nPoints_GL = (GLsizei) hull->nPoints;
+	hull->nHull_GL = (GLsizei) hull->nHull;
+	if (color==-1) {
+		hull->coordDraw = bov_points_new((GLfloat (*)[2]) hull->coord, hull->nPoints_GL, GL_DYNAMIC_DRAW);
+		hull->hullDraw = bov_order_new((GLuint*) hull->hull_idxs, hull->nHull_GL, GL_DYNAMIC_DRAW);
+	} else {
+		hull->coordDraw = bov_points_new((GLfloat (*)[2]) hull->coord, hull->nPoints_GL, GL_STATIC_DRAW);
+		hull->hullDraw = bov_order_new((GLuint*) hull->hull_idxs, hull->nHull_GL, GL_STATIC_DRAW);
+	}
+
+	GLfloat colorDraw[4];
+	colorDraw[3] = (GLfloat) 1.0;
+	if (color==-1) { hull->colorDraw = (GLfloat[4]) BLACK; }
+	else if (color==0) { hull->colorDraw = (GLfloat[4]) MEDIUMVIOLETRED; }
+	else if (color==1) { hull->colorDraw = (GLfloat[4]) FORESTGREEN; }
+	else if (color==2) { hull->colorDraw = (GLfloat[4]) FIREBRICK; }
+	else if (color==3) { hull->colorDraw = (GLfloat[4]) BLUE; }
+	else if (color==4) { hull->colorDraw = (GLfloat[4]) DARKORANGE; }
+	else if (color==5) { hull->colorDraw = (GLfloat[4]) DEEPPINK; }
+	else if (color==6) { hull->colorDraw = (GLfloat[4]) CHARTREUSE; }
+	else if (color==7) { hull->colorDraw = (GLfloat[4]) CRIMSON; }
+	else if (color==8) { hull->colorDraw = (GLfloat[4]) DEEPSKYBLUE; }
+	else if (color==9) { hull->colorDraw = (GLfloat[4]) GOLD; }
+	else if (color==10) { hull->colorDraw = (GLfloat[4]) HOTPINK; }
+	else if (color==11) { hull->colorDraw = (GLfloat[4]) MEDIUMSPRINGGREEN; }
+	else if (color==12) { hull->colorDraw = (GLfloat[4]) INDIANRED; }
+	else if (color==13) { hull->colorDraw = (GLfloat[4]) STEELBLUE; }
+	else if (color==14) { hull->colorDraw = (GLfloat[4]) PINK; }
+
+	bov_points_set_color(hull->coordDraw, hull->colorDraw);
+	bov_points_set_outline_color(hull->coordDraw, (GLfloat[4]) POINTS_OUTLINE_COLOR);
+	bov_points_set_width(hull->coordDraw, POINTS_WIDTH);
+	bov_points_set_outline_width(hull->coordDraw, POINTS_OUTLINE_WIDTH);
+	// bov_points_set_outline_width(hull->coordDraw, -1.0);
+}
+
+void convex_hull_update(struct convex_hull_t *hull, const int *idxs, int n)
+{
+	hull->nHull = n;
+	for (int i=0; i<n; i++)
+		hull->hull_idxs[i] = idxs[i];
+
+	if (hull->display) {
+		hull->nHull_GL = (GLsizei) n;
+		hull->hullDraw = bov_order_update(hull->hullDraw,
+											(GLuint*) idxs,
+											(GLsizei) n);
+	}
+}
+
+void convex_hull_partial_update(struct convex_hull_t *hull, const int *idxs, int start, int count, int newN)
+{
+	hull->nHull = newN;
+	for (int i=start; i<start+count; i++)
+		hull->hull_idxs[i] = idxs[i-start];
+
+	if (hull->display) {
+		hull->nHull_GL = (GLsizei) newN;
+		hull->hullDraw = bov_order_partial_update(hull->hullDraw,
+													(GLuint*) idxs,
+													(GLint) start,
+													(GLsizei) count,
+													(GLsizei) newN);
+	}
+}
---
+++ utils.h
@@ -0,0 +1,153 @@
+#include "BOV.h"
+
+#include <math.h>
+#include <time.h>
+
+/* ---------------------
+	Return the distance between x1 and x2
+--------------------- */
+float distance(const float x1[2], const float x2[2]);
+
+/* ---------------------
+	Return the argument of point in coord.
+	If it is not in coord, return -1.
+--------------------- */
+int find(int nPoints, float coord[][2], const float point[2]);
+
+/* ---------------------
+	Return the argument of the minimal value along axis.
+--------------------- */
+int argmin(int nPoints, float coord[][2], int axis);
+
+/* ---------------------
+	Return the argument of the maximal value along axis.
+--------------------- */
+int argmax(int nPoints, float coord[][2], int axis);
+
+/* ---------------------
+	Sort coord from its minimal value to its maximal one along axis.
+	Store in argsorted_list the arguments of elements such that coord is
+	sorted afterwards.
+--------------------- */
+void argsort(int nPoints, float coord[][2], int axis, int* argsorted_list);
+
+/* ---------------------
+	Return the argument of idx in vec.
+	If it is not in vec, return -1.
+--------------------- */
+int argfind(int nPoints, int *vec, int idx);
+
+/* ---------------------
+	Return the area of the triangle drawn by x1 → x2 → x3 → x1.
+--------------------- */
+double direction(float x1[2], float x2[2], float x3[2]);
+
+/* ---------------------
+	ANTOINE COMPLETE HERE !!
+--------------------- */
+struct sort
+{
+    float value;
+    int index;
+};
+int cmp(const void *a, const void *b);
+
+/*
+-------------------------------------------------
+                   CONVEX HULL
+-------------------------------------------------
+*/
+
+struct convex_hull_t{
+	double time; // Time needed to compute the current convex hull
+	char *method; // Method used to compute the current convex hull
+	int display; // Will we plot the points and the convex hull ? 1 if YES else 0
+
+	int Start; // Starting point of coordinates in a bigger coordinates vector (useful in chan algo)
+	int nPoints; // Amount of points in the current structure
+	float (*coord)[2]; // Coordinates of all points
+	GLsizei nPoints_GL;
+	bov_points_t *coordDraw;
+	GLfloat *colorDraw;
+
+	int nHull; // Amount of points in the current convex hull
+	int *hull_idxs; // Indexes of points (in coord[][2]) of points in the current convex hull (sorted !!)
+	GLsizei nHull_GL;
+	bov_order_t *hullDraw;
+};
+
+/*
+------------------------
+	Initialize a convex_hull_t structure from the coordinates coord[][2].
+	After its initialisation, the structure *hull has an empty hull_idxs
+	vector but all its points' coordinates have been initialized.
+	If display is 1, then we initialize the arguments which will allow us
+	to draw the points as well as the convex hull. Else display is 0.
+------------------------
+*/
+void convex_hull_init(struct convex_hull_t *hull,
+					  int start,
+					  int stop,
+					  float coord[][2],
+					  int display);
+
+/*
+------------------------
+	Initialize the drawing arguments of the convex_hull_t structure. This
+	function is whether called in convex_hull_init if display is 1 or
+	afterwards if we decide to show the convex hull later on.
+------------------------
+*/
+void convex_hull_display_init(struct convex_hull_t *hull,
+							  int color);
+
+/*
+------------------------
+	Update the hull_idxs vector of the convex_hull_t structure. The integer
+	'n' is the amount of element in the new convex hull and 'idxs' is its
+	new arguments in the vector of points coordinates of 'hull'.
+	It updates the convex hull bov_order_t if necessary (it means if display
+	is 1).
+------------------------
+*/
+void convex_hull_update(struct convex_hull_t *hull,
+						const int *idxs,
+						int n);
+
+/*
+------------------------
+	Partially update the hull_idxs vector of the convex_hull_t structure.
+	-→ DOES NOT WORK ACTUALLY !!
+------------------------
+*/
+void convex_hull_partial_update(struct convex_hull_t *hull,
+								const int *idxs,
+								int start,
+								int count,
+								int newN);
+
+
+// Display Characteristics
+#define POINTS_WIDTH 5e-3
+#define POINTS_OUTLINE_WIDTH 1e-4
+
+// Define Some Colors
+#define RGBMAX 255.0
+#define MAXCOLORS 15
+#define BLACK {0.0/RGBMAX, 0.0/RGBMAX, 0.0/RGBMAX, 1.0}
+#define MEDIUMVIOLETRED {199.0/RGBMAX, 21.0/RGBMAX, 133.0/RGBMAX, 1.0}
+#define FORESTGREEN {34.0/RGBMAX, 139.0/RGBMAX, 34.0/RGBMAX, 1.0}
+#define FIREBRICK {178.0/RGBMAX, 34.0/RGBMAX, 34.0/RGBMAX, 1.0}
+#define BLUE {0.0/RGBMAX, 0.0/RGBMAX, 255.0/RGBMAX, 1.0}
+#define DARKORANGE {255.0/RGBMAX, 99.0/RGBMAX, 71.0/RGBMAX, 1.0}
+#define DEEPPINK {255.0/RGBMAX, 20.0/RGBMAX, 147.0/RGBMAX, 1.0}
+#define CHARTREUSE {127.0/RGBMAX, 255.0/RGBMAX, 0.0/RGBMAX, 1.0}
+#define CRIMSON {220.0/RGBMAX, 20.0/RGBMAX, 60.0/RGBMAX, 1.0}
+#define DEEPSKYBLUE {0.0/RGBMAX, 191.0/RGBMAX, 255.0/RGBMAX, 1.0}
+#define GOLD {255.0/RGBMAX, 215.0/RGBMAX, 0.0/RGBMAX, 1.0}
+#define HOTPINK {255.0/RGBMAX, 105.0/RGBMAX, 180.0/RGBMAX, 1.0}
+#define MEDIUMSPRINGGREEN {0.0/RGBMAX, 250.0/RGBMAX, 154.0/RGBMAX, 1.0}
+#define INDIANRED {205.0/RGBMAX, 92.0/RGBMAX, 92.0/RGBMAX, 1.0}
+#define STEELBLUE {70.0/RGBMAX, 130.0/RGBMAX, 180.0/RGBMAX, 1.0}
+#define PINK {255.0/RGBMAX, 192.0/RGBMAX, 203.0/RGBMAX, 1.0}
+#define POINTS_OUTLINE_COLOR {0.0/RGBMAX,76.5/RGBMAX,30.6/RGBMAX, 0.25}
